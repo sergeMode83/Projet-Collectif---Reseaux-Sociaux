823efd4eca0fab5b8efd7c4dbc657fd04255d974
sergeMode83 <sergeMode83@users.noreply.github.com>
2025-07-08 10:14:21 +0000

    deploy: c4b4a5e5943588f5d54700147117f81f85d62100

---
+++ .lighthouserc.js
@@ -0,0 +1,10 @@
+module.exports = {
+  ci: {
+    collect: {
+      url: ['https://sergemode83.github.io/Projet-Collectif---Reseaux-Sociaux'],
+    },
+    upload: {
+      target: 'temporary-public-storage',
+    },
+  },
+};
